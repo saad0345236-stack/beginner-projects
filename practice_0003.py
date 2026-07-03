c33f046004390af78876f15819c46a202ba33f9e
saad0345236-stack <saad0345236@gmail.com>
2026-07-03 19:38:42 +0500

    Create practice_0003.py

---
+++ practice_0003.py
@@ -0,0 +1,32 @@
+# Using int & type functions:
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2026 - int(birth_year)
+print(type(age))
+print(age)
+# Exercise:
+weight = input('Weight (lbs): ')
+Kg = float(weight) * 0.45
+print(Kg)
+# Practicing with double & single quotes:
+line_1 = "Python is easy"
+line_2 = "Python isn't easy"
+email = '''
+Hi Michael,
+
+This is my first email.
+
+Thank You,
+XYZ.
+'''
+print(email)
+# Practing with colons:
+python = "Python is hard"
+python_2 = python[1:-1] 
+print(python_2)
+# More practices:
+sentence = "Yes and no"
+print(sentence[0])
+print(sentence[-1])
+print(sentence[0:7])
+print(sentence[5:])
