57716349a1effb5653c5970260d428ecfd2bf0b3
saad0345236-stack <saad0345236@gmail.com>
2026-07-05 19:47:53 +0500

    Create practice_0005.py

---
+++ practice_0005.py
@@ -0,0 +1,34 @@
+# Arithmetic operations:
+a = 10 + 5
+print(a)
+b = 15 - 5
+print(b)
+c = 20 * 5
+print(c)
+d = 25 / 5
+print(d)
+e = 30 // 5
+print(e)
+f = 35 ** 5
+print(f)
+g = 41 % 5
+print(g)
+# Using augmented increment operator:
+z = 23
+z %= 2
+print(z)
+y = (10 + 6) * 5 ** 5
+print(y)
+X = (5 + 5) * 10 - 5
+print(X)
+# Using math module and practicing with them:
+Y = -4.4
+Z = 5
+import math
+print(math.ceil(Y))
+print(math.floor(Y))
+print(math.fabs(Y))
+print(math.fmod(Z, Y))
+print(math.modf(Y))
+print(math.remainder(Z, Y))
+print(math.trunc(Y))
