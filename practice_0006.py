1cc89ef30883cd7a61fd0a8d6319fb6b297ca104
saad0345236-stack <saad0345236@gmail.com>
2026-07-06 19:36:40 +0500

    Create practice_0006.py

---
+++ practice_0006.py
@@ -0,0 +1,22 @@
+# Practicing with if, elif and else statements:
+snow_day = False
+sunny_day = True
+if snow_day:
+    print("It's a snow day!")
+    print("Wear a jacket.")
+elif sunny_day:
+    print("It's a sunny day!")
+    print("Drink water.")
+else:
+    print("It's a beautiful day.")
+print("Goodbye.")
+# More math modules:
+N = 30
+K = 20
+import math
+print(math.comb(N, K))
+print(math.factorial(N))
+print(math.gcd(N, K))
+print(math.isqrt(N))
+print(math.lcm(N, K))
+print(math.perm(N, K))
