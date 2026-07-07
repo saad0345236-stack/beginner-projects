892218e80424669aa12c74e6a7d47f6cf7487f75
saad0345236-stack <saad0345236@gmail.com>
2026-07-07 19:39:57 +0500

    Create practice_0007.py

---
+++ practice_0007.py
@@ -0,0 +1,21 @@
+# Exercise:
+house_cost = 1000000
+good_credit = False
+if good_credit:
+    put_down = house_cost * 0.1
+else:
+    put_down = house_cost * 0.2
+print(f"${put_down}")
+# Logical Operators:
+has_credit = True
+is_criminal = False
+if has_credit and not is_criminal:
+    print("Eligible for loan.")
+else:
+    print("Not Eligible.")
+# Comparaison Operators:
+age = 17
+if age > 17:
+    print("Eligible to vote.")
+else:
+    print("Not eligible to vote.")
