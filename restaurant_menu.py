84db15b2b627d4bc9037b9b450614402660bc0b7
saad0345236-stack <saad0345236@gmail.com>
2026-07-08 19:48:53 +0500

    Update restaurant_menu.py

--- restaurant_menu.py
+++ restaurant_menu.py
@@ -1,12 +1,12 @@
 # Offering a vegan and non–vegan menu:
 choice = input("Are you a vegan, yes or no? ")
-if choice == "yes":
+if choice.upper() == "yes":
     print("Here's the vegan options:")
     print("Hummus & pita")
     print("Falafel")
     print("Veggie stir–fry")
     print("Guacamole & chips")
-elif choice == "no":
+elif choice.upper() == "no":
     print("Here's the non–vegan options:")
     print("Burger")
     print("Chicken sandwich")
