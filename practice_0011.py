9ddb09184a52e04391c2585a127a0be3f41db6de
saad0345236-stack <saad0345236@gmail.com>
2026-07-11 19:38:00 +0500

    Create practice_0011.py

---
+++ practice_0011.py
@@ -0,0 +1,15 @@
+# Practicing For Loops:
+for letter in 'France':
+    print(letter)
+for item in ['John', 'Sara', 'Chris']:
+    print(item)
+for integer in [1, 3, 5, 7, 9]:
+    print(integer)
+for number in range (10, 17, 3):
+    print(number)
+for item_2 in ['France', 'Germany', 'Ireland']:
+    print(item_2)
+for digit in range (10, 20, 5):
+    print(digit)
+for character in 'name':
+    print(character)
