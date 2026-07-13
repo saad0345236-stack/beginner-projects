c42b5f873d5eb61daf25230832e5a68b04e91e72
saad0345236-stack <saad0345236@gmail.com>
2026-07-13 19:42:06 +0500

    Create holiday_list.py

---
+++ holiday_list.py
@@ -0,0 +1,15 @@
+print("Let's create a list of your top 3 favorite holidays. Please enter any 3 holidays below:")
+holiday_1 = input("1st Entry: ")
+holiday_2 = input("2nd Entry: ")
+holiday_3 = input("3rd Entry: ")
+print("Good choices!")
+question = input("Do you want to see the list? ").lower()
+if question == "yes":
+    print("This is your list:")
+    print(f"1. {holiday_1}")
+    print(f"2. {holiday_2}")
+    print(f"3. {holiday_3}")
+elif question == "no":
+    print("That's fine. Have a nice day!")
+else:
+    print("Sorry, I don't understand that.")
