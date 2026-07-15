da3aa3d45bee060396faa71486542b188297a9b4
saad0345236-stack <saad0345236@gmail.com>
2026-07-15 19:58:16 +0500

    Create practice_0015.py

---
+++ practice_0015.py
@@ -0,0 +1,17 @@
+# Creating a matrix list:
+matrix_list = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+matrix_list [2][1] = 99
+print(matrix_list [2][1])
+# Practing matrix list and for loops:
+list_2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+]
+for row in list_2:
+    for digit in row:
+        print(digit)
