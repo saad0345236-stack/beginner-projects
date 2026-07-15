3efd1f50f9404fb883501aa49313f6aaab6ed4fd
saad0345236-stack <saad0345236@gmail.com>
2026-07-15 19:57:08 +0500

    Create matrix_list.py

---
+++ matrix_list.py
@@ -0,0 +1,13 @@
+# Letting a user change 1 value in a matrix list:
+the_list = [
+    [3, 6, 9],
+    [12, 15, 18],
+    [21, 24, 27]
+]
+print(the_list)
+print("Change any 1 value from the given list.")
+row = int(input("Row index? "))
+column = int(input("Column index? "))
+change = int(input("What's the new value? "))
+the_list[row][number] = change
+print(the_list)
