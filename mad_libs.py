a67936bfcc231cec8b84fe966ab72351206e75cc
saad0345236-stack <saad0345236@gmail.com>
2026-07-31 19:39:13 +0500

    Create mad_libs.py

---
+++ mad_libs.py
@@ -0,0 +1,16 @@
+# Doing Mad libs:
+print("Make a random sentence:")
+
+adjective = input("Adjective: ")
+noun = input("Noun: ")
+verb = input ("Verb: ")
+
+print(f"On vacation, the {adjective} {noun} did {verb} before swimming.")
+print("Interesting... How about another one?")
+
+adj2 = input("Adjective: ")
+noun2 = input("Noun: ")
+verb2 = input("Verb: ")
+place = input("Place: ")
+
+print(f"The {adj2} {noun2} decided to {verb2} today at the {place}.")
