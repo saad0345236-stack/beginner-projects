0cd37e94ab12fcf3e77b107b8ff612157f6492a6
saad0345236-stack <saad0345236@gmail.com>
2026-08-01 20:31:41 +0500

    Create practice_0032.py

---
+++ practice_0032.py
@@ -0,0 +1,15 @@
+# Practicing ascii_lowecase:
+import string
+
+letters = string.ascii_lowercase
+print(letters)
+
+import random
+
+print(random.choice(letters))
+
+print(string.ascii_lowercase.isalpha())
+print(string.ascii_lowercase.index('p'))
+print(string.ascii_lowercase.upper())
+print(len(string.ascii_lowercase))
+print(string.ascii_lowercase.find('a'))
