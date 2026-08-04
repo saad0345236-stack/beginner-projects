191f93a992a96e106e158b06edd1800859b29019
saad0345236-stack <saad0345236@gmail.com>
2026-08-04 20:39:47 +0500

    Create practice_0035.py

---
+++ practice_0035.py
@@ -0,0 +1,11 @@
+# Practicing with a list:
+tasks = []
+tasks.append('Cooking')
+tasks.append('Cleaning')
+tasks.append('Watering the planets')
+print(tasks)
+for integer in range(10):
+    print(integer)
+tasks.remove('Cooking')
+tasks.append('Shopping')
+print(tasks)
