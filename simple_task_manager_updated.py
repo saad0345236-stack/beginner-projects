06c2eadfe3263e8ab06b4341a52e519d65ee0944
saad0345236-stack <saad0345236@gmail.com>
2026-08-05 20:27:58 +0500

    Update simple_task_manager_updated.py

--- simple_task_manager_updated.py
+++ simple_task_manager_updated.py
@@ -7,14 +7,21 @@ try:
         app.append(added)
         delete = input("Want to delete something? (y/n): ").lower()
         if delete == 'y':
-            ask = input("What? ")
-            app.remove(ask)
+            ask = int(input("What? (1 based number): "))
+            app.pop(ask - 1)
         else:
             pass
         mark = input("Do you want to mark a task as done? (y/n): ").lower()
         if mark == 'y':
-            task = int(input("Which one (index)? "))
-            app[task] = app[task] + '(DONE)'
+            task = int(input("Which one (1 based number)? "))
+            app[task - 1] = app[task] + '(DONE)'
+        else:
+            pass
+        edit = input("Do you want to edit a task? (y/n): ").lower()
+        if edit == 'y':
+            editing = int(input("Which one? (1 based number): "))
+            change = input("To what? ")
+            app[editing - 1] = change
         else:
             pass
         ask = input("Do you want to add another task? (y/n): ").lower()
