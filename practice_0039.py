6eea5a878d1795f12b05a53c8d0f730b8857e608
saad0345236-stack <saad0345236@gmail.com>
2026-08-08 20:23:15 +0500

    Create practice_0039.py

---
+++ practice_0039.py
@@ -0,0 +1,12 @@
+# Practicing drill:
+word = 'operation'
+print(len(word))
+print('o' in word)
+print('s' in word)
+print(word.upper())
+print(word.lower())
+print(word.capitalize())
+new_word = input("Enter word: ")
+print(len(new_word))
+letter = input("Enter letter: ").lower()
+print(letter in new_word)
