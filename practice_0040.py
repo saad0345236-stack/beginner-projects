e01413cf933ed33da30f2fcbb98ba4a14f45ae22
saad0345236-stack <saad0345236@gmail.com>
2026-08-09 20:29:41 +0500

    Create practice_0040.py

---
+++ practice_0040.py
@@ -0,0 +1,11 @@
+# Practicing Drill:
+times2 = lambda number: number * 2
+print(times2(2))
+print(times2(10))
+print(times2(3))
+sqauring = lambda digit: digit ** 2
+print(sqauring(4))
+print(sqauring(6))
+print(sqauring(2))
+add = lambda x, y: x + y
+print(add(2, 4))
