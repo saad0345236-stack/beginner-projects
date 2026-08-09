3ba872bfc566db2b6a68ee24c8eed345965c9bd4
saad0345236-stack <saad0345236@gmail.com>
2026-08-09 20:21:31 +0500

    Create inventory_management_system_v1.py

---
+++ inventory_management_system_v1.py
@@ -0,0 +1,25 @@
+# Inventory management system of a store:
+try:
+    products = []
+    while True:
+        menu = input("MAIN MENU: 'add product', 'check products' or 'quit': ").lower()
+        if menu == 'add product':
+            name = input("Name: ")
+            price = input("Price: ")
+            products.append(name)
+            products.append(': $')
+            products.append(price)
+            products.append('|')
+            print("Current inventory:")
+            print(''.join(products))
+        elif menu == 'check products':
+            print("Here's the inventory:")
+            print(''.join(products))
+        elif menu == 'quit':
+            print("You left the menu.")
+            break
+        else:
+            print("Invalid, you're going to be redirected to the main menu.")
+            pass
+except ValueError:
+    print("Invalid.")
