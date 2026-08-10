079543f8caf2b2bcae570ce1cf30ce68f4f34a2c
saad0345236-stack <saad0345236@gmail.com>
2026-08-10 20:36:08 +0500

    Create practice_0041.py

---
+++ practice_0041.py
@@ -0,0 +1,11 @@
+# Practicing Drill:
+def greet(name):
+    print(f"Hi, {name}!")
+    def sentence():
+        print("It's nice to meet you.")
+        def handshake():
+            print("Shaking hands")
+        handshake()
+    sentence()
+
+greet("Bob")
