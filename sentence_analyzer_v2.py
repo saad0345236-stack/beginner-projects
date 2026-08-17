cbb6c7eb4b7c7a6ce67e1f56e1f9d9a709197519
saad0345236-stack <saad0345236@gmail.com>
2026-08-17 20:34:14 +0500

    Create sentence_analyzer_v2.py

---
+++ sentence_analyzer_v2.py
@@ -0,0 +1,22 @@
+# Sentence analyzer:
+def sentence_analyzer(sentence):
+    words = sentence.split()
+
+    utterance = []
+    for word in words:
+        utterance.append(word)
+    
+    total = len(utterance)
+    longest = ''
+    
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+    
+    result = f"Total number of words in the sentence are {total}. The largest word is {longest}. The sentence in all caps is '{sentence.upper()}' and sentence in lower caps is '{sentence.lower()}'."
+
+    return print(result)
+
+sentence = input("Enter sentence: ").lower()
+
+sentence_analyzer(sentence)
