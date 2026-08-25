215da6b90b177644dec9daafd91f52741de00366
saad0345236-stack <saad0345236@gmail.com>
2026-08-25 20:18:08 +0500

    Create practice_0055.py

---
+++ practice_0055.py
@@ -0,0 +1,13 @@
+# Practicing Drill:
+def greet():
+    print("Hi! Nice to meet you.")
+
+def talk():
+    print("Talk")
+
+def say_bye():
+    print("Bye! See you later!")
+
+greet()
+talk()
+say_bye()
